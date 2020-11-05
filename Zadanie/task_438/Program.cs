1c6b3168e354de67139df661ad3609ea22319230
w-kubica <62216498+w-kubica@users.noreply.github.com>
2020-11-05 15:07:32 +0100

    Init

---
+++ Zadanie/task_438/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_438
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

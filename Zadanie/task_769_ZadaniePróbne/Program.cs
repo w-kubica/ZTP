a0bbca010227f12e185a9a1f1d5f94986e83baa9
Weronika Kubica <w.kubicaweronika@gmail.com>
2020-11-19 21:56:08 +0100

    Done_769

---
+++ Zadanie/task_769_ZadaniePróbne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_769_ZadaniePróbne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}

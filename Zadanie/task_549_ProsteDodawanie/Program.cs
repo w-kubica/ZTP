5cb84c9ab70409191d39675a7c23e1e2abf7446b
Weronika Kubica <w.kubicaweronika@gmail.com>
2020-12-03 13:39:49 +0100

    Update tasks

---
+++ Zadanie/task_549_ProsteDodawanie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _549_ProsteDodawanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTests = int.Parse(Console.ReadLine());
+
+            if (numberOfTests > 0 && numberOfTests < 80)
+            {
+                for (int i = 0; i < numberOfTests; i++)
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    string cyfry = Console.ReadLine();
+                    string[] tab = cyfry.Split(" ");
+
+                    int suma = 0;
+                    foreach (string v in tab)
+                    {
+                        int cyfra = int.Parse(v);
+                        suma += cyfra;
+                    }
+                    Console.WriteLine(suma);
+                }
+            }
+        }
+    }
+}
